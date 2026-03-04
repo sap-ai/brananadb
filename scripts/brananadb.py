c150a1f0f5d3c8c6c99f2154d549609d20e8175c
sap-ai <adriangerleit@gmail.com>
2026-03-03 22:28:33 -0500

    Allowed more than one host and port for the database
    Add error handling for SERVER_HOST and SERVER_PORT initialization

--- scripts/brananadb.py
+++ scripts/brananadb.py
@@ -1,4 +1,10 @@
 import webbrowser
-SERVER_HOST = '0.0.0.0'
-SERVER_PORT = 8000
+try:
+   print(SERVER_HOST)
+except:
+   SERVER_HOST = '0.0.0.0'
+try:
+   print(SERVER_PORT)
+except:
+   SERVER_PORT = 8000
 l = f'http://{SERVER_HOST}:{SERVER_PORT}'
