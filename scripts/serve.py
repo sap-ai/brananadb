0dad43de5c7e16ca2ddf21af7b294f1466667500
sap-ai <adriangerleit@gmail.com>
2026-03-03 23:48:40 -0500

    Set default file name to 'd.txt' in serve.py

--- scripts/serve.py
+++ scripts/serve.py
@@ -10,7 +10,7 @@ except:
 try:
    print(fi)
 except:
-   fi = ""
+   fi = "d.txt"
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server_socket.bind((SERVER_HOST, SERVER_PORT))
